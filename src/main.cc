a9c129c06849b853d7d60f8fa3859ac46877430b
Cuiyere <40125653+Cuiyere@users.noreply.github.com>
2018-10-21 22:21:12 +0200

    Clean Up
    Useless files deletes
    Code can generate "picture.ppm"

--- src/main.cc
+++ src/main.cc
@@ -3,21 +3,29 @@
  15.10.2018
  Arthur Bierlaire
  */
+
 #include<fstream>
-#include<iostream>
-#include<string>
+
 using namespace std;
 
 int main () {
+	const int width = 255, height = 255;
 
-	//system(("mkdir -p .//Images"));
-	ofstream flux;
-	string filename = "picture.ppm";
-	flux.open(filename, ios::out);
+	ofstream img ("picture.ppm");
+	img << "P3" << endl;
+	img << width << " " << height << endl;
+	img << "255" << endl;
 
-	if (flux) { cout << "Bonjour, je suis une image." << endl; }
-	else { cout << "Bite" << endl; }
+	for (int i = 0; i < height; i++) {
+		for (int j = 0; j < width; j++) {
+			int r = i % 255;
+			int g = j % 255;
+			int b = i * j % 255;
 
-	flux.close();
+			img << r << " " << g << " " << b << endl;
+		}
+	}
+
+	system("open picture.ppm");
 	return 0;
 }
